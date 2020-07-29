25ac3b28ae3ef6b4ef562bd55f3244ea65265124
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-29 19:37:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 array_names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-room_numbers = [1,2,3,4,5,6,7]
+room_numbers = [0,1,2,3,4,5,6,7]
 
 def badge_maker(name)
   "Hello, my name is #{name}."
