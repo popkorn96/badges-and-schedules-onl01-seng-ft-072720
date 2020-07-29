afd881c08fd49fbfe7da2fedb763504e7da7c4c4
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-29 18:55:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,6 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-array_names = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
-room_numbers = [1,2,3,4,5,6,7]
-
 def batch_badge_creator(array_names)
   array_names.each do |name|
   end
@@ -13,6 +10,8 @@ end
 
 def assign_rooms(room_numbers)
   room_numbers.each do |num|
+    array_names = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+room_numbers = [1,2,3,4,5,6,7]
    "Hello, #{name}! You will be assigned to room #{num}!"
 end
 end
