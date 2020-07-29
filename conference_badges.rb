8d94800c35daa4af364b9e469aa905212f3a6fde
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-29 18:58:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 array_names = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
-def badge_maker(name)
-  "Hello, my name is #{name}."
+def badge_maker(array_names)
+  "Hello, my name is #{array_names}."
 end
 
 def batch_badge_creator(array_names)
