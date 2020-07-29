409415b90dc4d495a3518682c51eecf9f33ecd51
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-29 19:57:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,9 +23,9 @@ end
 
 def printer(batch_badge_creator)
   batch_badge_creator.each do |array_names|
-  puts batch_badge_creator
+  puts batch_badge_creator(array_names)
 end
   assign_rooms.each do |assign_rooms|
-    puts assign_rooms
+    puts assign_rooms(assign_rooms)
   end
 end
