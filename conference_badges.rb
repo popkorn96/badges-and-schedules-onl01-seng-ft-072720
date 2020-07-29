2891b91f2386442e0b826b548770b393f4c6c0e8
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-29 18:46:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,8 @@ array_names = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
 room_numbers = [1,2,3,4,5,6,7]
 
 def batch_badge_creator(array_names)
-  array_names.each do |name,num|
+  array_names.each do |name|
+    room_numbers.each do |num|
     puts "Hello, #{name}! You will be assigned to room #{num}!"
   end
 end
